76433d1b8964acdb32aaaab84a8f64e2b4fa0852
Jefferson Doria <jarruda93@gmail.com>
2023-03-26 16:15:21 -0300

    otimização de imagem

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,9 +1,12 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-const cssmin = require('gulp-cssmin')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify')
+//list dependences
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
+//css
 function tarefasCSS(cb) {
     return gulp.src('./vendor/**/*.css')
         .pipe(concat('libs.css'))
@@ -12,6 +15,7 @@ function tarefasCSS(cb) {
         .pipe(gulp.dest('./dist/css'))
 }
 
+//js
 function tarefasJS(){
     return gulp.src('./vendor/**/*.js')
         .pipe(concat('libs.js'))
@@ -20,5 +24,17 @@ function tarefasJS(){
         .pipe(gulp.dest('./dist/js'))
 }
 
+//images
+function tarefasImagem(){
+    return gulp.src('./src/images/*.{jpg,png}')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality:80, progressive: true}),
+        imagemin.optipng({optimizationLevel: 2}),
+
+    ]))
+    .pipe(gulp.dest('./dist/images'))
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefasImagem
