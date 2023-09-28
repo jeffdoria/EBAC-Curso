bf2094835d2392fe0e5ec42781d5c77242952642
​Jefferson Doria dos Santos <jefferson.santos@ADM.UNIP.BR>
2023-09-28 18:28:05 -0300

    feat: add icon e  melhorias de código

--- Modulo-29/src/modulesImport.js
+++ Modulo-29/src/modulesImport.js
@@ -1,10 +1,9 @@
-// import bootstrap from 'bootstrap'
-// import * as nomeQualquer from './modulesExport'
-import Cachorro from './classes'
+/* import bootstrap from 'bootstrap'
+ import * as nomeQualquer from './modulesExport'*/
+
+import Cachorro from './classes.js';
 
 const cachorro = new Cachorro('cachorro')
-animais.falar()
-animais.comer()
-animais.dormir()
-
-//Erro 404 no arquivo Imports instlawr o http-server e testar em casa
+cachorro.falar()
+cachorro.comer()
+cachorro.dormir()
